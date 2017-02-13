12116643094fef298408ca5eeb373f1fca1fdd26
Travis Caro <travisbcaro@gmail.com>
2017-02-13 11:36:32 -0500

    Refactor GOL game structure

--- gol.js
+++ gol.js
@@ -15,8 +15,9 @@ function App(gol) {
 
   // TBC : Properties
   self.gol;
-  self.canvasManager;
   self.elements = { };
+  self.context = { };
+  self.colors = { cell: 'yellow', background: 'black' };
 
   // TBC : Methods
   self.initialize = function() {
@@ -31,23 +32,20 @@ function App(gol) {
     self.elements.btnGameState.setToPause = function() { $(this).html('pause'); };
     self.elements.btnGameState.setToResume = function() { $(this).html('resume'); };
 
+    self.context = self.elements.canvas[0].getContext('2d');
+
     self.bindEventHandlers();
   }
 
   self.bindEventHandlers = function() {
     // TBC : New Game loads new game instance and starts it
     self.elements.btnNewGame.on('click', function() {
-      self.gol = new GameOfLife({
+      self.gol = new GameOfLife();
+
+      self.gol.seed({
         interval: self.getInterval(),
         size: self.elements.canvas[0].width / self.getSize()
       });
-
-      self.canvasManager = new CanvasManager({
-        canvas: self.elements.canvas[0], // TBC : [0] removes jQuery wrselfer
-        size: self.getSize()
-      });
-
-      self.gol.seed();
       self.elements.btnGameState.setToStart();
     });
 
@@ -68,33 +66,52 @@ function App(gol) {
     });
 
     $(document).on('gol:seed', function() {
-      self.drawCells();
+      self.clearCanvas();
+      self.drawLivingCells();
       self.reportStats();
     });
 
     $(document).on('gol:tick', function() {
-      self.drawCells();
+      self.clearCanvas();
+      self.drawLivingCells();
       self.reportStats();
     });
   }
 
-  self.drawCells = function() {
-    self.canvasManager.clearCanvas();
-    self.gol.rows.forEach(function(column, rowIdx) {
-      column.forEach(function(cell, colIdx) {
-        if (cell.isAlive) {
-          self.canvasManager.drawCell(rowIdx, colIdx);
-        }
-      });
+  self.drawLivingCells = function() {
+    var cellOffset = 0,
+        size = self.getSize();
+
+    if (size !== 1) {
+      cellOffset = size / 5;
+    }
+
+    self.context.fillStyle = self.colors.cell;
+    self.context.beginPath();
+    self.gol.eachCell(function(cell) {
+      if(cell.isAlive()) {
+        self.context.fillRect(
+          cell.position.col * size + cellOffset,
+          cell.position.row * size + cellOffset,
+          size - cellOffset,
+          size - cellOffset
+        );
+      }
     });
+    self.context.closePath();
+  }
+
+  self.clearCanvas = function() {
+    self.context.fillStyle = self.colors.background;
+    self.context.clearRect(0, 0, self.elements.canvas[0].width, self.elements.canvas[0].height);
   }
 
   self.reportStats = function() {
     self.elements.statsTable.empty();
-    for(key in self.gol.statistics) {
+    for(key in self.gol.stats) {
       var statRow = $('<tr>'),
-          label = self.gol.statistics[key].label,
-          value = self.gol.statistics[key].value;
+          label = self.gol.stats[key].label,
+          value = self.gol.stats[key].value;
 
       if (typeof value === 'number') {
         value = value.toLocaleString();
@@ -129,48 +146,6 @@ function App(gol) {
 }
 
 /*
-  TBC : The CanvasManager will be responsible for all output to the canvas.
-
-  This object will be responsible for handling the actual drawing of the game objects.
-  The CanvasManager is the delegated worker for all actual drawing output.
-*/
-
-function CanvasManager(options) {
-  var self = { };
-
-  self.canvas = options.canvas;
-  self.context = self.canvas.getContext('2d');
-
-  self.size = options.size;
-  self.size === 1 ? self.cellOffset = 0 : self.cellOffset = self.size / 5;
-  self.cellColor = options.cellColor || 'yellow';
-  self.backgroundColor = options.backgroundColor || 'black';
-
-  self.drawCell = function(row, column) {
-    self.context.fillStyle = self.cellColor;
-    var position = {
-      x: column * self.size + self.cellOffset,
-      y: row * self.size + self.cellOffset
-    }
-
-    self.context.moveTo(position.x, position.y);
-    self.context.fillRect(
-      position.x,
-      position.y,
-      self.size - self.cellOffset,
-      self.size - self.cellOffset
-    );
-  }
-
-  self.clearCanvas = function() {
-    self.context.fillStyle = self.backgroundColor;
-    self.context.fillRect(0, 0, self.canvas.width, self.canvas.height);
-  }
-
-  return self;
-}
-
-/*
   TBC : The GameOfLife object will be responsible for the actual GameOfLife simulation.
 
   This object will be responsible for handling the actual game rules and serve as the
@@ -178,7 +153,11 @@ function CanvasManager(options) {
   and not be tied to the canvas or the GUI in any way. It should simply trigger
   events and serve as the game's logical manager.
 */
-function GameOfLife(options) {
+function GameOfLife() {
+  var DEFAULT_SIZE = 600,
+      DEFAULT_INTERVAL = 1000,
+      DEFAULT_PERCENT_CELL_GENERATION_CHANCE = 10;
+
   var self = { };
 
   self.GAME_STATES = {
@@ -187,12 +166,15 @@ function GameOfLife(options) {
     PAUSED: 'paused'
   }
 
-  self.size = options.size || 600;
-  self.interval = options.interval || 1000;
+  self.size = DEFAULT_SIZE;
+  self.interval = DEFAULT_INTERVAL;
+  self.cellGenerationChance = DEFAULT_PERCENT_CELL_GENERATION_CHANCE;
   self.gameState = self.GAME_STATES.NEW_GAME;
+
   self.rows = [];
+  self.livingCells = { };
   self.intervalId;
-  self.statistics = {
+  self.stats = {
     generations: {
       label: 'generations',
       value: 0
@@ -207,26 +189,52 @@ function GameOfLife(options) {
     }
   }
 
-  self.seed = function() {
+  self.eachCell = function(callback) {
+    for (var i = 1; i < self.size - 1; i++) {
+      for (var j = 1; j < self.size - 1; j++) {
+        var cell = self.rows[i][j];
+        callback(cell);
+      }
+    }
+  }
+
+  self.seed = function(options) {
+    self.size = options.size || DEFAULT_SIZE;
+    self.interval = options.interval || DEFAULT_INTERVAL;
+    self.cellGenerationChance = options.cellGenerationChance || DEFAULT_PERCENT_CELL_GENERATION_CHANCE;
+
     for (var i = 0; i < self.size; i++) {
       var columns = [];
 
       for (var j = 0; j < self.size; j++) {
         var cell = new Cell();
-        cell.randomizeInitialState();
+        cell.position.row = i;
+        cell.position.col = j;
 
-        if (cell.isAlive) {
-          self.statistics.livingCells.value++;
+        // TBC : Seed dead cell border
+        if (i === 0 || j === 0 || i === self.size || j === self.size) {
+          cell.die();
+        } else {
+          self.randomizeInitialCellState(cell);
+
+          if (cell.isAlive()) self.stats.livingCells.value++;
         }
-
         columns[j] = cell;
       }
-
-      self.rows.push(columns);
+      self.rows[i] = columns;
     }
 
-    self.statistics.totalCells.value = self.size * self.size;
+    self.eachCell(function(cell) {
+      self.determineNextCellState(cell);
+    });
+
+    self.stats.totalCells.value = self.size * self.size;
     $(document).trigger('gol:seed');
+  }
+
+  self.randomizeInitialCellState = function(cell) {
+    var random = Math.floor(Math.random() * 100);
+    random < self.cellGenerationChance ? cell.live() : cell.die();
   }
 
   self.run = function() {
@@ -240,64 +248,58 @@ function GameOfLife(options) {
   }
 
   self.tick = function() {
-    self.statistics.livingCells.value = 0;
-
-    self.rows.forEach(function(columns, rowIdx) {
-      columns.forEach(function(cell, colIdx) {
-        var livingNeighbors = self.countLivingNeighbors(rowIdx, colIdx);
-
-        if (cell.isAlive) {
-          if (livingNeighbors < 2 || livingNeighbors > 3) {
-            cell.die();
-          } else {
-            cell.survive();
-          }
-        } else if (livingNeighbors === 3) {
-          cell.generate();
-        }
-
-        if (cell.isAlive) {
-          self.statistics.livingCells.value++;
-        }
-      });
+    self.eachCell(function(cell){
+      self.updateCellState(cell);
     });
 
-    self.statistics.generations.value++;
+    self.eachCell(function(cell){
+      self.determineNextCellState(cell);
+    });
+
+    self.stats.generations.value++;
     $(document).trigger('gol:tick');
   }
 
-  self.countLivingNeighbors = function(rowIdx, colIdx) {
-    var offsets = {
-          NW: { rowOffset: -1, colOffset: -1 },
-          N: { rowOffset: -1, colOffset: 0 },
-          NE: { rowOffset: -1, colOffset: 1 },
-          E: { rowOffset: 0, colOffset: 1 },
-          SE: { rowOffset: 1, colOffset: 1 },
-          S: { rowOffset: 1, colOffset: 0 },
-          SW: { rowOffset: 1, colOffset: -1 },
-          W: { rowOffset: 0, colOffset: -1 }
-        },
+  self.getCellNeighbors = function(cell) {
+    var p = cell.position,
+        neighbors = [];
+
+    neighbors.push( self.rows[ p.row - 1 ][ p.col - 1 ] ); // NW
+    neighbors.push( self.rows[ p.row - 1 ][ p.col ] );     // N
+    neighbors.push( self.rows[ p.row - 1 ][ p.col + 1 ] ); // NE
+    neighbors.push( self.rows[ p.row ][ p.col + 1 ] );     // E
+    neighbors.push( self.rows[ p.row + 1 ][ p.col + 1 ] ); // SE
+    neighbors.push( self.rows[ p.row + 1 ][ p.col ] );     // S
+    neighbors.push( self.rows[ p.row + 1 ][ p.col - 1 ] ); // SW
+    neighbors.push( self.rows[ p.row ][ p.col - 1 ] );     // W
+
+    return neighbors;
+  }
+
+  self.updateCellState = function(cell) {
+    self.stats.livingCells.value = 0;
+    cell.state = cell.nextState;
+    if (cell.isAlive()) self.stats.livingCells.value++;
+  }
+
+  self.determineNextCellState = function(cell) {
+    var neighbors = self.getCellNeighbors(cell),
         count = 0;
 
-    for (direction in offsets) {
-      var rowOffset = offsets[direction].rowOffset,
-          colOffset = offsets[direction].colOffset;
+    neighbors.forEach(function(neighbor) {
+      if (neighbor.isAlive()) count++;
 
-      if (
-        rowIdx + rowOffset >= 0 &&
-        colIdx + colOffset >= 0 &&
-        rowIdx + rowOffset < self.size &&
-        colIdx + colOffset < self.size
-      ) {
-          var neighbor = self.rows[rowIdx + rowOffset][colIdx + colOffset];
-
-          if (neighbor.isAlive) {
-            count++;
-          }
+      if (count > 3) {
+        cell.dieNext();
+        return;
       }
-    }
+    });
 
-    return count;
+    if (cell.isAlive()) {
+      (count === 2 || count === 3) ? cell.liveNext() : cell.dieNext()
+    } else if (count === 3) {
+      cell.liveNext();
+    }
   }
 
   return self;
@@ -312,27 +314,31 @@ function GameOfLife(options) {
 function Cell() {
   var self = { };
 
-  self.age = 0;
-  self.isAlive = false;
+  self.STATES = { ALIVE: 1, DEAD: 0 };
 
-  self.generate = function() {
-    self.isAlive = true;
-  }
+  self.position = { row: 0, col: 0 };
+  self.age = 0;
+  self.state = self.STATES.DEAD;
+  self.nextState = self.STATES.DEAD;
+
+  self.isAlive = function() { return (self.state === self.STATES.ALIVE) ? true : false }
+
+  self.live = function() { self.state = self.STATES.ALIVE; }
+
+  self.liveNext = function() { self.nextState = self.STATES.ALIVE; }
+
+  self.age = function() { self.age++; }
+
+  self.dieNext = function() { self.nextState = self.STATES.DEAD; }
 
   self.survive = function() {
-    self.age++;
+    self.liveNext();
+    self.age();
   }
 
   self.die = function() {
+    self.state = self.STATES.DEAD;
     self.age = 0;
-    self.isAlive = false;
-  }
-
-  self.randomizeInitialState = function() {
-    // TBC : percentChance represents what percent chance the cell has to become animated on inital load
-    var percentChance = 10;
-    var random = Math.floor(Math.random() * 100);
-    random < percentChance ? self.generate() : self.die();
   }
 
   return self;
